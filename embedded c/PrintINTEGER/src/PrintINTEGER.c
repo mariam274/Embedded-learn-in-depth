d794f04d50022a60a531b9978a6e132be61720d4
mariam274 <mariamm.wael26@gmail.com>
2024-08-21 23:29:50 +0300

    Add files via upload

---
+++ embedded c/PrintINTEGER/src/PrintINTEGER.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : PrintINTEGER.c
+ Author      : marioma
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x;
+	printf("Enter a number:");
+	scanf("%d", &x);
+	printf("Your entered value: %d", x);
+
+
+}
